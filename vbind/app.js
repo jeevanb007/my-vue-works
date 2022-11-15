3454b3af0c50806e465ea751a825bdcf7e7ca6f5
jeevanb007 <kittub01@gmail.com>
2022-11-15 14:25:44 +0530

    first project is completed bases on the concept of v-bind

---
+++ vbind/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            product: 'socks',
+            altText: 'A pair of socks',
+            image: "./assets/socks.jpg"
+        }
+    }
+})
